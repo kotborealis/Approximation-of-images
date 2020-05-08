4a0f4dd6cbe6b97cee015252d498a62650844b5c
Hollowfications <45915761+Hollowfications@users.noreply.github.com>
2020-05-08 14:45:49 +0300

    Add files via upload

--- source/shape.js
+++ source/shape.js
@@ -18,3 +18,43 @@ export class Shape {
 	}
 
 }
+
+class Polygon extends Shape {
+	constructor(w, h, count) {
+		this.computeBbox();
+	}
+
+	render(ctx) {
+		ctx.beginPath();
+		this.points.forEach(([x, y], index) => {
+			if (index) {
+				ctx.lineTo(x, y);
+			} else {
+				ctx.moveTo(x, y);
+			}
+		});
+		ctx.closePath();
+		ctx.fill();
+	}
+
+	computeBbox() {
+		let min = [
+			this.points.reduce((v, p) => Math.min(v, p[0]), Infinity),
+			this.points.reduce((v, p) => Math.min(v, p[1]), Infinity)
+		];
+		let max = [
+			this.points.reduce((v, p) => Math.max(v, p[0]), -Infinity),
+			this.points.reduce((v, p) => Math.max(v, p[1]), -Infinity)
+		];
+
+		this.bbox = {
+			left: min[0],
+			top: min[1],
+			width: (max[0]-min[0]) || 1,
+			height: (max[1]-min[1]) || 1
+		};
+
+		return this;
+	}
+
+}

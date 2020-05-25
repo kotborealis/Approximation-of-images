8f2578474d42599d8b220bf6f3b174e01172bc63
Hollowfications <45915761+Hollowfications@users.noreply.github.com>
2020-05-26 02:50:44 +0300

    shape classes added

--- source/shape.js
+++ source/shape.js
@@ -1,5 +1,5 @@
 import Canvas from "./canvas.js";
-import * as util from "./util.js";
+import * as util from "./utility.js";
 
 export class Shape {
 	static randomPoint(width, height) {
@@ -57,4 +57,61 @@ class Polygon extends Shape {
 		return this;
 	}
 
+}
+export class Triangle extends Polygon {
+	constructor(w, h) {
+		super(w, h, 3);
+	}
+}
+
+export class Rectangle extends Polygon {
+	constructor(w, h) {
+		super(w, h, 4);
+	}
+
+	_createPoints(w, h, count) {
+		let p1 = Shape.randomPoint(w, h);
+		let p2 = Shape.randomPoint(w, h);
+
+		let left = Math.min(p1[0], p2[0]);
+		let right = Math.max(p1[0], p2[0]);
+		let top = Math.min(p1[1], p2[1]);
+		let bottom = Math.max(p1[1], p2[1]);
+
+		return [
+			[left, top],
+			[right, top],
+			[right, bottom],
+			[left, bottom]
+		];
+	}
+}
+
+export class Ellipse extends Shape {
+	constructor(w, h) {
+		super(w, h);
+
+		this.center = Shape.randomPoint(w, h);
+		this.rx = 1 + ~~(Math.random() * 20);
+		this.ry = 1 + ~~(Math.random() * 20);
+
+		this.computeBbox();
+	}
+
+	render(ctx) {
+		ctx.beginPath();
+		ctx.ellipse(this.center[0], this.center[1], this.rx, this.ry, 0, 0, 2*Math.PI, false);
+		ctx.fill();
+	}
+
+
+	computeBbox() {
+		this.bbox = {
+			left: this.center[0] - this.rx,
+			top: this.center[1] - this.ry,
+			width: 2*this.rx,
+			height: 2*this.ry
+		}
+		return this;
+	}
 }
